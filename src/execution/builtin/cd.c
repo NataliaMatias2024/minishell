66b4fdbf30ce6661765b5185f2c19e8cc8a994fb
namatias <namatias@student.42sp.org.br>
2026-02-07 15:42:44 -0300

    feat(builtin): finalize cd implementation with environment sync
    Implemented full cd logic including PWD/OLDPWD updates, support for special paths, and fixed linked list traversal to ensure memory safety and prevent duplicate nodes.

--- src/execution/builtin/cd.c
+++ src/execution/builtin/cd.c
@@ -6,19 +6,19 @@
 /*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 12:18:15 by namatias          #+#    #+#             */
-/*   Updated: 2026/02/07 02:00:37 by namatias         ###   ########.fr       */
+/*   Updated: 2026/02/07 15:32:32 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static	char *target_path(t_environment *env, char **args);
+static	char *target_path(t_environment *env, char **args, char *old_pwd);
+static	int	execute_chdir(char *target, char *old_pwd);
 
 int	builtin_cd(t_environment *env, char **args)
 {
 	char	pwd[SIZE_PATH];
 	char	*old_pwd;
-	char	*new_pwd;
 	char	*target;
 
 	//se receber info a + retornar erro de muitos argumentos
@@ -27,39 +27,27 @@ int	builtin_cd(t_environment *env, char **args)
 		ft_putendl_fd("minishell: cd: too many arguments", STDERR_FILENO);
 		return (1);
 	}
-
 	//verificar o local atual e salvar(caminho antigo)
 	if(getcwd(pwd, SIZE_PATH))
 		old_pwd = ft_strdup(pwd);
 	else
 		old_pwd = ft_strdup("");
-	target = target_path(env, args);
+	//verificar se diretorio indicado existe e salvar seu caminho
+	target = target_path(env, args, old_pwd);
 	if (!target)
 		return (1);
-
-	//chamar chdir(objetivo), pois ele procura o nome no ambiente retornando 0 no sucesso e -1 no erro
-	if (chdir(target) != 0)
-	{
-			perror("minishell:  cd");
-			return (1);
-	}
-	if (args[1] && ft_strcmp(args[1], "-") == 0)
-		ft_printf("%s\n", old_pwd);
-
-	if (getcwd(pwd, SIZE_PATH))
-		new_pwd = ft_strdup(pwd);
-	else
-		new_pwd = ft_strdup("");
-
-	//atualizar ambiente, manter o caminho antigo pego antes do chdir e o novo caminho pwd
+	//chamar chdir(objetivo), pois ele procura o nome ou caminho no ambiente retornando 0 ou -1 (erro)
+	if (execute_chdir(target, old_pwd) != 0)
+		return (1);
+	//atualizar ambiente e dar free no caminho antigo (antes do chdir) e att o novo (após chdir)
 	create_update_list_env(&env, "OLDPWD", old_pwd);
 	free(old_pwd);
-	create_update_list_env(&env, "PWD", new_pwd);
-	free(new_pwd);
+	if (getcwd(pwd, SIZE_PATH))
+		create_update_list_env(&env, "PWD", pwd);
 	return (0);
 }
 
-static	char *target_path(t_environment *env, char **args)
+static	char *target_path(t_environment *env, char **args, char *old_pwd)
 {
 	//verificar se recebemos cd + nome de onde ir
 	//se receber apenas cd OU  cd ~ mudar para home (getenv("HOME"))
@@ -83,8 +71,21 @@ static	char *target_path(t_environment *env, char **args)
 			ft_putendl_fd("minishell: cd: OLDPWD not set", 2);
 			return (NULL);
 		}
+		ft_printf("%s\n", old_pwd);
 	}
 	else
 		target = args[1];
 	return (target);
+}
+
+static	int	execute_chdir(char *target, char *old_pwd)
+{
+	//se retornar qualquer coisa q nao 0 é pq deu erro, imprimir erro na tela, dar free e finalizar
+	if (chdir(target) != 0)
+	{
+		perror("minishell:  cd");
+		free(old_pwd);
+		return (1);
+	}
+	return (0);
 }
